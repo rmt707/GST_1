46b6c95f550e279943b8bd617f5d15a2cd89df72
Raquel <rmt707@alumnos.unican.es>
2022-03-28 12:23:38 +0200

    ++

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int MainFunctionality (int i){
 
 int main (){
 	int ret = 0;
-	for (int = 0; i < 10; i++){
+	for (int i = 0; i < 10; i++){
 		//Main functionality check
 		if(MainFunctionality(i) == i){
 			printf("SUCCESS: main functionality %d\n", i);
