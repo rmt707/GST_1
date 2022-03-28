c7bb3e654d6f35c7c84f8231940a09532946930c
Raquel <rmt707@alumnos.unican.es>
2022-03-28 12:10:38 +0200

    ++

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int MainFunctionality (int i){
+	return i;
+}
+
 int main (){
-	printf("Hello world"\n);
-	printf("Cambios en main"\n);
-	printf("Cambios en funcionalidad1"\n);
+	int ret = 0;
+	for (int = 0; i < 10; i++){
+		//Main functionality check
+		if(MainFunctionality(i) == i){
+			printf("SUCCESS: main functionality %d"\n, i);
+		} else {
+			printf("FAILED: main functionality %d\n", i);
+			ret = -1;
+		}
+	}
+	return ret;
 }
